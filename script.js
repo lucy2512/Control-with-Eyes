c8569b762374d5b9106ce61187c572cdffc67525
lucy2512 <mail4parthib@gmail.com>
2021-08-23 11:47:57 +0530

    Images Loaded

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 window.saveDataAcrossSessions = true
+let imageElement = getNewImage()
+let nextImageElement = getNewImage(true)
 
 
 webgazer.setGazeListener((data, timestamp) => {
     console.log(data, timestamp)
 })
 .begin()
+
+function getNewImage(next = false){
+    const img = document.createElement("img")
+    img.src="https://picsum.photos/1000?" + Math.random()
+    if(next) img.classList.add("next")
+
+    document.body.append(img)
+    return img
+}
